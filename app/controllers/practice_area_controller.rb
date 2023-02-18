84fecca1a2082e05a8ed02ffc5047f34d17c69fa
Bhavyansh <bhavyansh001@gmail.com>
2023-02-19 00:41:37 +0530

    Generating random numbers and operator

--- app/controllers/practice_area_controller.rb
+++ app/controllers/practice_area_controller.rb
@@ -1,11 +1,17 @@
 class PracticeAreaController < ApplicationController
-  before_action :random_nums, only: [:index]
+  before_action :random_nums
+  before_action :operator
   def index
-    
+    @expression = (@num1 + @operator + @num2)
+    @ans = eval("#{@expression}")
   end
   private
   def random_nums
-    @num1 = (Random.rand(50) + 50).to_s
-    @num2 = Random.rand(50).to_s
+    @num1 = Random.rand(20).to_s
+    @num2 = Random.rand(10).to_s
+  end
+  def operator
+    op_array = ['+', '-', '*', '/']
+    @operator = op_array.sample
   end
 end
