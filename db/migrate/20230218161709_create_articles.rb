5f779550235676e68a1a37123db726f0bf2e4464
Bhavyansh <bhavyansh001@gmail.com>
2023-02-18 21:50:39 +0530

    Added Article model

---
+++ db/migrate/20230218161709_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :articles, :user_id
+  end
+end
