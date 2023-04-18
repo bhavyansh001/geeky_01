b3d816f858efb70334b0656bd6f1bddf4e5f8895
Bhavyansh <bhavyansh001@gmail.com>
2023-04-18 14:01:42 +0530

    Beginning working on Bro vs Bro mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_055651) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_082604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_055651) do
     t.index ["user_id"], name: "index_level_up_sessions_on_user_id"
   end
 
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_participants_on_room_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "expression"
     t.string "solution"
@@ -95,6 +104,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_055651) do
     t.boolean "is_correct", default: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -116,5 +130,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_055651) do
   add_foreign_key "level_up_session_questions", "level_up_sessions"
   add_foreign_key "level_up_session_questions", "questions"
   add_foreign_key "level_up_sessions", "users"
+  add_foreign_key "participants", "rooms"
+  add_foreign_key "participants", "users"
   add_foreign_key "questions", "users"
 end
