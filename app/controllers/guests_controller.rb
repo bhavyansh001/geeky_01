c75748d0e489a61a56e1d02a732f85ef40d43483
Bhavyansh <bhavyansh001@gmail.com>
2023-05-09 10:43:14 +0530

    Removing the guest model

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,8 +1,8 @@
 require 'faker'
 class GuestsController < ApplicationController
   def create
-    guest = Guest.create(email: Faker::Internet.email, password: Devise.friendly_token.first(8))
-    sign_in(:guest, guest)
+    guest = User.create(email: Faker::Internet.email, password: Devise.friendly_token.first(8), guest: true)
+    sign_in(:user, guest)
     redirect_to root_path
   end
 end
