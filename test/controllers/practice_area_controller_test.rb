f02f1ac0de06080113cdd4bdbf3b835f20d89116
Bhavyansh <bhavyansh001@gmail.com>
2023-06-09 11:43:37 +0530

    Deleted test folder since testing using RSpec

--- test/controllers/practice_area_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PracticeAreaControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get practice_area_index_url
-    assert_response :success
-  end
-end
