0051417789f7140c8f0a641634f7fb0bf32f160f
Bhavyansh <bhavyansh001@gmail.com>
2024-03-07 13:08:38 +0530

    Multiple changes in views and asset files

---
+++ app/assets/config/importmap.rb
@@ -0,0 +1,8 @@
+Importmap.draw do
+    # Map your JS files
+    pin "js.js", to: "assets/javascript/js.js"
+    pin "jquery", to: "assets/javascript/jquery.js"
+  
+    # Map your CSS file
+    pin "css.css", to: "assets/stylesheets/css.css"
+  end
