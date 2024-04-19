67d1c6e1e75711bcdda8c6e478e91666c055b711
Bhavyansh <bhavyansh001@gmail.com>
2024-04-19 18:50:48 +0530

    design improvements

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,21 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+const copyBtn = document.getElementById('copy-btn');
+const roomUrlInput = document.getElementById('room-url');
+
+copyBtn.addEventListener('click', function() {
+  roomUrlInput.select();
+  roomUrlInput.setSelectionRange(0, 99999); // For mobile devices
+  document.execCommand('copy');
+  // Change the button text to "Copied"
+  copyBtn.textContent = 'Copied';
+  // Optionally, revert the button text after a few seconds
+  setTimeout(function() {
+    copyBtn.textContent = 'Copy URL';
+  }, 2000); // 2000 milliseconds = 2 seconds
+});
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
