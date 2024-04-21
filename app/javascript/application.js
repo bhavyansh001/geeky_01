3d847c6a2be9bd480685f5d03c6b55b56f72395a
Bhavyansh <bhavyansh001@gmail.com>
2024-04-21 21:37:37 +0530

    Refactored js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,20 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener('turbo:load', function() {
+    const copyBtn = document.getElementById('copy-btn');
+    const roomUrlInput = document.getElementById('room-url');
+   
+    if (copyBtn && roomUrlInput) {
+       copyBtn.addEventListener('click', function() {
+         roomUrlInput.select();
+         roomUrlInput.setSelectionRange(0, 99999);
+         document.execCommand('copy');
+         copyBtn.textContent = 'Copied';
+         setTimeout(function() {
+           copyBtn.textContent = 'Copy URL';
+         }, 2000);
+       });
+    }
+   });
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,23 +6,6 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-document.addEventListener('turbo:load', function() {
-  const copyBtn = document.getElementById('copy-btn');
-  const roomUrlInput = document.getElementById('room-url');
- 
-  if (copyBtn && roomUrlInput) {
-     copyBtn.addEventListener('click', function() {
-       roomUrlInput.select();
-       roomUrlInput.setSelectionRange(0, 99999);
-       document.execCommand('copy');
-       copyBtn.textContent = 'Copied';
-       setTimeout(function() {
-         copyBtn.textContent = 'Copy URL';
-       }, 2000);
-     });
-  }
- });
-
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
