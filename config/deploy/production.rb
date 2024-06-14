56c59183d1a8672e1227cde608d79d6824c045ec
Bhavyansh <bhavyansh001@gmail.com>
2024-06-14 19:50:11 +0530

    Using remote postgresql connection with app

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
-server '100.27.190.133', user: 'deploy', roles: %w{app db web}
+server '3.80.117.98', user: 'deploy', roles: %w{app db web}
 # role-based syntax
 # ==================
 
