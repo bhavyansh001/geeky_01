d2903297ceb50c293c42319504b6ebb503243732
Bhavyansh <bhavyansh001@gmail.com>
2024-06-27 20:06:29 +0530

    Configuring prometheus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    Prometheus::Counter.new(
-      name: 'products_controller_index_requests_total',
-      help: 'Total number of requests to the index action'
-    ).increment
   end
 
   def about
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
-server '54.157.59.80', user: 'deploy', roles: %w{app db web}
+server '54.226.61.118', user: 'deploy', roles: %w{app db web}
 # role-based syntax
 # ==================
 
