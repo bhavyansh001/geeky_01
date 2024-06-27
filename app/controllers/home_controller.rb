d446cf0011f3c0eee91628d5ad1ca0667b08f717
Bhavyansh <bhavyansh001@gmail.com>
2024-06-27 20:24:31 +0530

    Configuring prometheus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
+require 'prometheus/client'
+
 class HomeController < ApplicationController
+
+  counter = Prometheus::Client.registry.counter('home_controller_index_requests_total', 'Total number of requests to the home controller index action')
   def index
+    counter.increment
   end
 
   def about
