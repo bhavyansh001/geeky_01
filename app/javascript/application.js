5ff777d3e15cc0f64b5de8e4d74b898bc33406d6
Bhavyansh <bhavyansh001@gmail.com>
2024-09-07 01:53:57 +0530

    fix: ActionCable fixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,4 +20,12 @@ document.addEventListener('turbo:load', function() {
        });
     }
    });
-import "channels"
+
+// Import and register all your controllers from the importmap under controllers/*
+import { application } from "controllers/application"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+
+import "./channels"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   root 'home#index'
   get 'home/about'
   devise_for :users, controllers: { sessions: 'users/sessions'}
