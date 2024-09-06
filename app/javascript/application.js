65bb8115e9f92e995d9f6e6c97361ba3ab6e279e
Bhavyansh <bhavyansh001@gmail.com>
2024-09-07 02:14:48 +0530

    fix: ActionCable fixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "channels"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-import "./channels"
 
 document.addEventListener('turbo:load', function() {
     const copyBtn = document.getElementById('copy-btn');
